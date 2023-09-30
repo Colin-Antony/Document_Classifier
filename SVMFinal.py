4eb498d70be3506469e3a8b45a57509221e5b71c
Colin Antony <colinantonyk@gmail.com>
2023-09-30 16:08:41 +0530

    Including verbose and saving svm model

--- SVMFinal.py
+++ SVMFinal.py
@@ -1,3 +1,4 @@
+import pickle
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -9,39 +10,39 @@ from sklearn.pipeline import Pipeline
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.metrics import classification_report, accuracy_score
 
-# Fetching the data and splitting
+# Getting and splitting data
 newsgroups_data = fetch_20newsgroups(subset='all', random_state=42)
 X = newsgroups_data.data
 y = newsgroups_data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
 
-# NLP preprocessing steps
+# NLP preprocessing
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
 
 
 def preprocess_text(text):
-    # Convert to lowercase
+
     text = text.lower()
-    # Remove non-alphanumeric characters
+    # Remove non-alphanumeric
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text, re.I|re.A)
     # Tokenize text
     tokens = nltk.word_tokenize(text)
     # Remove stop words
     tokens = [t for t in tokens if t not in stop_words]
-    # Lemmatize tokens
+    # Lemmatization
     tokens = [lemmatizer.lemmatize(t) for t in tokens]
     # Rejoin tokens into a string
     text = ' '.join(tokens)
     return text
 
 
-# Defining the pipeline
+# Define the pipeline
 svm_classifier = Pipeline([
     ("Preprocess_Tfidf", TfidfVectorizer(preprocessor=preprocess_text,
                                          ngram_range=(1,2), max_df=0.75)),
-    ("SVM", SVC(kernel="linear", C=10))
+    ("SVM", SVC(kernel="linear", C=10, verbose=True))
 ])
 
 # Fitting the pipeline
@@ -59,3 +60,6 @@ accuracy = accuracy_score(y_test, predicted)
 print(f"Accuracy: {accuracy}")
 print(classification_report(y_test, predicted,
                             target_names=newsgroups_data.target_names))
+
+filename = "SVM_Text_Classifier.sav"
+pickle.dump(svm_classifier, open(filename,'wb'))
